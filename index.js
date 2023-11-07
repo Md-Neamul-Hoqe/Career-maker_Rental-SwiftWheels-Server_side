e56db3558b0f9fff0589517fea7f7ec6c9d855ab
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-11-07 07:23:52 +0600

    db credentials hide by dot env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -6,12 +7,62 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+// console.log(process.env.swiftWheels_USER, process.env.swiftWheels_PASS, process.env.swiftWheels_DB);
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.swiftWheels_USER}:${process.env.swiftWheels_PASS}@carsdoctordb.pehv7ki.mongodb.net/${process.env.swiftWheels_DB}?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const SW_database = client.db(process.env.swiftWheels_DB);
+        const carCollection = SW_database.collection('cars');
+        const bikeCollection = SW_database.collection('bikes');
+        const bookingCollection = SW_database.collection('bookings');
+        const serviceCollection = SW_database.collection('services');
+
+        // Send a ping to confirm a successful connection
+        await client.db(process.env.swiftWheels_DB).command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('SwiftWheels Server is running...')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`SwiftWheels app listening on port ${port}`)
 })
